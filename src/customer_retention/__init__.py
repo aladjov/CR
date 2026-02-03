f0dc03954ef3542026df4ef09a79bfa397b4e6ea
Hristo Aladjov <h.aladjov@hyperaspect.com>
2026-02-03 12:11:38 -0500

    Bump version to 0.76.1a1

--- src/customer_retention/__init__.py
+++ src/customer_retention/__init__.py
@@ -17,7 +17,7 @@ Main module categories:
                 llm_context, iteration)
 """
 
-__version__ = "0.76.0a3"
+__version__ = "0.76.1a1"
 
 # Environment utilities (always available)
 from .core.compat import (
