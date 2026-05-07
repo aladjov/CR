36d8f8af4a612f1bf00b8058fec9088411247c18
Hristo Aladjov <h.aladjov@hyperaspect.com>
2026-05-07 13:42:03 +0300

    Bump version to 1.02.7a4

--- src/customer_retention/__init__.py
+++ src/customer_retention/__init__.py
@@ -17,7 +17,7 @@ Main module categories:
                 llm_context, iteration)
 """
 
-__version__ = "1.02.7a3"
+__version__ = "1.02.7a4"
 
 # Environment utilities (always available)
 from .core.compat import (
